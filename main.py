8db0a0c853e7aebc0e88004fd0b936184e30c6f4
Homespeaker <142523511+Homespeaker@users.noreply.github.com>
2024-08-13 19:01:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 import telebot
+import qrcode
 from data import *  
 
 bot = telebot.TeleBot('6182241691:AAFl3lahEdNLQGp3hurvMI8JeYbAIRlHc54')
@@ -7,8 +8,32 @@ bot = telebot.TeleBot('6182241691:AAFl3lahEdNLQGp3hurvMI8JeYbAIRlHc54')
 @bot.message_handler(commands=['start'])
 def start(message):
     user_reg(message.from_user.id)
-    bot.send_message(message.chat.id, "Обзор, инструкция, обучение и контакты на этом сайте:\n"
-                     "https://ellic.tilda.ws/")
+    bot.send_message(message.chat.id, "Привет, это видео инструкция. Первое видео - как поворачивать катаясь - Поворот цапли.")
+    video = open('tsaplya.mp4', 'rb')
+    bot.send_video(message.chat.id, video)
+    bot.next_step_handler(message, step2)
+
+def 2step(message):
+    bot.send_message(message.chat.id, "Второе видео - как разворачиваться на месте - Робот Вертер.")
+    video = open('verter.MOV', 'rb')
+    bot.send_video(message.chat.id, video)
+    bot.next_step_handler(message, step3)
+
+def 3step(message):
+    bot.send_message(message.chat.id, "Будте аккуратны, внимательно прочитайте правила безопасности:")
+    photo = open('rules.jpg', 'rb')
+    bot.send_photo(message.chat.id, photo)
+    bot.next_step_handler(message, step4)
+
+def 4step(message):
+    bot.send_message(message.chat.id, "Вот ваш допуск, покажите его стюарду:")
+    data = f"{message.from_user.id}"
+    filename = f"{message.from_user.id}.png"
+    img = qrcode.make(data)
+    bot.send_photo(message.chat.id, img)
+    img.save(filename)
+    
+    
     
 @bot.message_handler(commands=['clear_base'])
 def clear(message):
